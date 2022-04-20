7adc85e9e2c1b91a149d92a88494cbe0b7c15f28
lenaglas <lena.glas@web.de>
2022-04-20 12:58:43 +0200

    Add settings file

--- src/App.vue
+++ src/App.vue
@@ -1,156 +1,156 @@
 <template>
-  <body>
-    <div>
-      <!-- layout prior exercise: prompts user to login -->
-      <div class="is-vhcentered has-text-centered pt-6" style="height: 100%">
-        <div class="columns is-hcentered mb-5">
-          <img
-            class="image is-hcentered"
-            style="width: 100px"
-            src="./assets/rocket.svg"
-          />
-        </div>
+<body>
+  <div>
+    <!-- layout prior exercise: prompts user to login -->
+    <div class="is-vhcentered has-text-centered pt-6" style="height: 100%">
+      <div class="columns is-hcentered mb-5">
+        <img class="image is-hcentered" style="width: 100px" src="./assets/iceberg.png" />
+      </div>
 
-        <div v-if="!VMAssigned">
-          <div class="is-json title ">
-            Welcome to SOCCyberRange!
-            <div class="subtitle mb-6 ">A Cyber Range for SOC Analysts.</div>
-          </div>
-        </div>
-
-        <div v-if="!checked" class="subtitle mb-6 is-6 ">
-          Before registration, please take part in our pre-quiz
-          <!--strong><a class="subtitle is-4 is-json has-text-link" href="https://quizizz.com/join?gc=07101609" target="_blank"><u>
-pre-quiz</u></a></strong-->
-          , then come back to this page.
-
-          <br />
-          Please use your <strong>NDS-account (e.g. glr02738)</strong> to
-          register for the quiz. <br />
-
-         
-
-          <div class="buttons is-centered pt-6 " v-if="!quizStarted">
-            <button
-              class="button submit-button is-rounded "
-              type="submit"
-              value="Submit"
-              @click="
-                //proceedToQuiz();
-                quizStarted = true;
-              "
-              v-if="!VMsTaken"
-            >
-              <span>Start Quiz &#10140;</span>
-            </button>
-          </div>
-
-          <iframe v-if="quizStarted"
-                src="https://quizizz.com/join?gc=15953433"
-                style="display: block; width: 100%; height: 50vh"
-                class="pt-6"
-              ></iframe>
-
-          <div v-if="quizStarted">
-            <input
-              type="checkbox"
-              class="subtitle is- is-json mt-6"
-              id="checkbox"
-              v-model="checked"
-            />
-            <label class="subtitle is-3 is-json" for="checkbox">
-              <strong> Quiz Completed</strong>
-            </label>
-          </div>
-               
-
-        </div>
-
-        <div v-if="checked && VMAssigned">
-          <div class="title pt-6">
-            Hello
-            <strong class="has-text-primary"> {{ this.pseudonym }} </strong>.
-          </div>
-
-          <div class="small">
-            (this will be your pseudonym on the cyber range)
-          </div>
-          <br />
-          Thank you for registration. We registered your user ID
-          <strong class="has-text-primary"> {{ this.userID }} </strong> and set
-          up the cyber range for you.
-
-          <div class="buttons is-centered mt-5">
-            <button
-              class="button submit-button is-rounded mt-5"
-              type="submit"
-              value="Submit"
-              @click="proceedToCR()"
-            >
-              <span>PROCEED TO CYBER RANGE → </span>
-            </button>
-          </div>
-        </div>
-
-        <br />
-
-        <div stlye="width=100%">
-          <div v-if="checked && !VMAssigned" class="subtitle is-5 mb-6">
-            Thank you for taking part in the pre-quiz! <br />Now you can
-            <strong>register</strong> for the cyber range <br />
-            <br />
-            <br />
-
-            <form @submit.prevent="validateId()" v-if="!VMAssigned">
-              <div class="columns is-offset-one-quarter">
-                <input
-                  class="input input-label-long is-size-6 is-centered column"
-                  :value="'Your NDS Account: '"
-                />
-                <span>
-                  <input
-                    class="input input-short is-size-6 blank-input column"
-                    v-model="userID"
-                    :placeholder="'e.g. glr02738'"
-                  />
-                </span>
-              </div>
-
-              <div class="has-text-danger" v-if="emptyInput">
-                User ID cannot be empty.
-              </div>
-              <div class="has-text-danger" v-if="userExists">
-                User ID was already used.
-              </div>
-              <div class="has-text-danger" v-if="VMsTaken">
-                No free Cyber Range environments at the moment. <br />
-                Please contact one of the trainers.
-              </div>
-
-              <br />
-              <div class="buttons is-centered ">
-                <button
-                  class="button submit-button is-rounded "
-                  type="submit"
-                  value="Submit"
-                  @click="validateId()"
-                  v-if="!VMsTaken"
-                >
-                  <span>Register</span>
-                </button>
-              </div>
-            </form>
-          </div>
+      <div v-if="!VMAssigned">
+        <div class="is-json title">
+          Welcome to {{scenarioName}}!
+          <div class="subtitle mb-6">A Cyber Range for SOC Analysts.</div>
         </div>
       </div>
 
+      <div v-if="!checked" class="subtitle mb-6 is-6">
+        Before registration, please take part in our pre-quiz
+        <!--strong><a class="subtitle is-4 is-json has-text-link" href="https://quizizz.com/join?gc=07101609" target="_blank"><u>
+        pre-quiz</u></a></strong-->
+        , then come back to this page.
+        <br />Please use your
+        <strong>{{naming}} ({{namingPlaceholder}})</strong> to
+        register for the quiz.
+        <br />
+
+        <div class="buttons is-centered pt-6" v-if="!quizStarted">
+          <button
+            class="button submit-button is-rounded"
+            type="submit"
+            value="Submit"
+            @click="
+                //proceedToQuiz();
+                quizStarted = true;
+              "
+            v-if="!VMsTaken"
+          >
+            <span>Start Quiz &#10140;</span>
+          </button>
+        </div>
+
+        <iframe
+          v-if="quizStarted"
+          :src="urlPreQuiz"
+          style="display: block; width: 100%; height: 50vh"
+          class="pt-6"
+        ></iframe>
+
+        <div v-if="quizStarted" class="mt-5">
+          Nothing to see? Click
+          <strong>
+            <a class="has-text-link" :href="urlPreQuiz" target="_blank">
+              <u>here</u>
+            </a>
+          </strong> and come back to this page when you have finished.
+        </div>
+
+        <div class="buttons is-centered pt-6" v-if="quizStarted">
+          <button
+            class="button submit-button is-rounded"
+            type="submit"
+            value="Submit"
+            @click="checked = true"
+            v-if="!VMsTaken"
+          >
+            <span>Quiz Completed &#10004;</span>
+          </button>
+        </div>
+      </div>
+
+      <div v-if="checked && VMAssigned">
+        <div class="title pt-6">
+          Hello
+          <strong class="has-text-primary">{{ this.pseudonym }}</strong>.
+        </div>
+
+        <div class="small">(this will be your pseudonym on the cyber range)</div>
+        <br />Thank you for registration. We registered your user ID
+        <strong
+          class="has-text-primary"
+        >{{ this.userID }}</strong>
+        and set
+        up the cyber range for you.
+        <div class="buttons is-centered mt-5">
+          <button
+            class="button submit-button is-rounded mt-5"
+            type="submit"
+            value="Submit"
+            @click="proceedToCR()"
+          >
+            <span>PROCEED TO CYBER RANGE →</span>
+          </button>
+        </div>
+      </div>
+
+      <br />
+
+      <div stlye="width=100%">
+        <div v-if="checked && !VMAssigned" class="subtitle is-5 mb-6">
+          Thank you for taking part in the pre-quiz!
+          <br />Now you can
+          <strong>register</strong> for the cyber range
+          <br />
+          <br />
+          <br />
+
+          <form @submit.prevent="validateId()" v-if="!VMAssigned">
+            <div class="columns is-offset-one-quarter">
+              <input
+                class="input input-label-long is-size-6 is-centered column"
+                :value="'Your '+ naming + ': '"
+              />
+              <!-- Change Back -->
+              <span>
+                <input
+                  class="input input-short is-size-6 blank-input column"
+                  v-model="userID"
+                  :placeholder="namingPlaceholder"
+                />
+              </span>
+            </div>
+
+            <div class="has-text-danger" v-if="emptyInput">User ID cannot be empty.</div>
+            <div class="has-text-danger" v-if="userExists">User ID was already used.</div>
+            <div class="has-text-danger" v-if="VMsTaken">
+              No free Cyber Range environments at the moment.
+              <br />Please contact one of the trainers.
+            </div>
+
+            <br />
+            <div class="buttons is-centered">
+              <button
+                class="button submit-button is-rounded"
+                type="submit"
+                value="Submit"
+                @click="validateId()"
+                v-if="!VMsTaken"
+              >
+                <span>Register</span>
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
-  </body>
+  </div>
+</body>
 </template>
 
 <script>
 import { userDashboard } from "@/firebase"; // TODO rename to userScoreboard
 import { VM_db } from "@/firebase";
+import settings from "./Settings.js";
 
 export default {
   name: "App",
@@ -169,14 +169,14 @@ export default {
       blockly: null,
       checked: false,
       quizStarted: false,
+      scenarioName: settings.scenarioName,
+      urlPreQuiz: settings.urlPreQuiz,
+      naming: settings.naming,
+      namingPlaceholder: settings.namingPlaceholder,
     };
   },
 
   methods: {
-    proceedToQuiz() {
-      window.open("https://quizizz.com/join?gc=15953433", "_balnk").focus();
-    },
-
     proceedToCR() {
       window.open(this.url, "_self");
     },
@@ -213,9 +213,7 @@ export default {
     },
 
     async getFreeVM() {
-      const snapshot = await VM_db.where("userID", "==", "")
-        .limit(1)
-        .get();
+      const snapshot = await VM_db.where("userID", "==", "").limit(1).get();
 
       this.VMData = snapshot.docs.map((doc) => doc.data());
 
@@ -239,7 +237,7 @@ export default {
           ip: this.VMData[0].ip,
           pseudonym: this.VMData[0].pseudonym,
           userID: this.userID,
-          blockly: this.blockly
+          blockly: this.blockly,
         });
 
         this.pseudonym = this.VMData[0].pseudonym;
@@ -321,9 +319,7 @@ export default {
     },   */
 
   async getVM() {
-    const snapshot = await VM_db.where("userID", "==", "")
-      .limit(1)
-      .get();
+    const snapshot = await VM_db.where("userID", "==", "").limit(1).get();
     console.log(snapshot.docs.map((doc) => doc.data()));
     return JSON.stringify(snapshot.docs.map((doc) => doc.data().pseudonym)); //funktioniert so nicht
   },
---
+++ src/Settings.js
@@ -0,0 +1,10 @@
+const settings = {
+
+    urlPreQuiz: "https://quizizz.com/join?gc=66705525",
+    scenarioName: "ICEBERG",
+    naming: "name",
+    namingPlaceholder: "e.g. mglas"
+
+}
+
+export default settings;
