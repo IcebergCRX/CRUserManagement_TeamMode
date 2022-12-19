4a45646072ded8e865d6ffb9424a7725d0ad575b
lenaglas <lena.glas@web.de>
2022-12-19 09:56:38 +0100

    wip

--- src/App.vue
+++ src/App.vue
@@ -398,6 +398,7 @@ export default {
       groupMemberTwo: null,
       groupMemberThree: null,
       round: null,
+      points: null,
       emptyInput: false,
       emptyGroupMembers: false,
       groupMembersRegistered: false,
